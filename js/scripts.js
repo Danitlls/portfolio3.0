3d562143bb0838836e2c6688253c6202f6d040ed
Daniela Trulls <danielatrulls01@gmail.com>
2017-06-16 16:43:14 -0700

    Add comment with animation fadeinUp

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
     $(this).toggleClass("is-active");
   });
   // $(".intro").toggleUp(800);
+
+
+
+// Hook doAnimations on scroll, and trigger a scroll
+$(window).on('scroll', function(){
+  $(".block1").removeClass("animatable").addClass("animated");
+  $(window).trigger('scroll');
+
+});
 });
